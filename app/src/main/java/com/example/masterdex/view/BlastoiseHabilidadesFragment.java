7185088ff17475ff0e65077f190c35b480176851
Erik Antunes <erik.antunes@live.com>
2019-11-26 21:24:03 -0300

    MasterDex 1.5

--- app/src/main/java/com/example/masterdex/view/BlastoiseHabilidadesFragment.java
+++ app/src/main/java/com/example/masterdex/view/BlastoiseHabilidadesFragment.java
@@ -1,18 +1,15 @@
 package com.example.masterdex.view;
-
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
 import com.example.masterdex.R;
 
 public class BlastoiseHabilidadesFragment extends Fragment {
+
+    private ImageView easterEgg;
 
     public BlastoiseHabilidadesFragment() {
         // Required empty public constructor
@@ -21,12 +18,12 @@ public class BlastoiseHabilidadesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blastoise_habilidades, container, false);
 
-        View view = inflater.inflate(R.layout.fragment_habilidades, container, false);
 
         return view;
     }
-        }
+}
 
 
 
